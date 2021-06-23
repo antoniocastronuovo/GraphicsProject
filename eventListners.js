c2b1051e181406470ec7ad36ac050ca970d0247e
giacomocassano <48253515+giacomocassano@users.noreply.github.com>
2021-06-23 13:31:47 +0200

    Added inputs for choosing colors of lights

--- eventListners.js
+++ eventListners.js
@@ -26,6 +26,7 @@
     
     //hideSameLocation();
     setCameraListeners();
+    setAmbientColorsListeners();
 
     var moveFrom = document.getElementById("drop-down-from");
     moveFrom.addEventListener("change", onFromChange, false);
@@ -83,7 +84,6 @@ function setCameraListeners(){
 
 
 function setMouseListeners(){
-    //Event handlers to rotate camera on mouse dragging
     var fromRod = null;
     var mouseState = false;
     var lastMouseX = -100, lastMouseY = -100;
@@ -189,6 +189,30 @@ function setMouseListeners(){
 	window.addEventListener("mousemove", doMouseMove, false);
 }
 
+function setAmbientColorsListeners() {
+    function hexToRGB(c) {
+        if(/^#([a-f0-9]{3}){1,2}$/.test(c)){
+            if(c.length== 4){
+                c= '#'+[c[1], c[1], c[2], c[2], c[3], c[3]].join('');
+            }
+            c= '0x'+c.substring(1);
+            return [ (c>>16)&255, (c>>8)&255, c&255];
+        }
+        return '';
+    }
+    document.getElementById("lowerColor").addEventListener("input", e => {
+        var rgb = hexToRGB(e.target.value);
+        ambientLightLowColor = [rgb[0]/255, rgb[1]/255, rgb[2]/255];
+    }, false);
+
+    document.getElementById("upperColor").addEventListener("input", e => {
+        var rgb = hexToRGB(e.target.value);
+        ambientLightUpColor = [rgb[0]/255, rgb[1]/255, rgb[2]/255];
+    }, false);
+
+    
+}
+
 function hideSameLocation(){
     var moveFrom = document.getElementById("drop-down-from");
     var moveTo = document.getElementById("drop-down-to");
@@ -285,3 +309,4 @@ function onFromChange() {
     }
 
 }
+
